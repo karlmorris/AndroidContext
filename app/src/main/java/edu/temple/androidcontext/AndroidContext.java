13d4a8f2a989fcbb8f6209312cc814ee6505a749
Karl Morris <siliclone@gmail.com>
2015-10-31 11:18:33 -0400

    Using Universal Image Loader for funamic image loading.

---
+++ app/src/main/java/edu/temple/androidcontext/AndroidContext.java
@@ -0,0 +1,28 @@
+package edu.temple.androidcontext;
+
+import android.app.Application;
+
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+public class AndroidContext extends Application{
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        // Create image loader configuration with global context
+        ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(this);
+        config.threadPriority(Thread.NORM_PRIORITY - 2);
+        config.denyCacheImageMultipleSizesInMemory();
+        config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
+        config.diskCacheSize(50 * 1024 * 1024); // 50 MiB
+        config.tasksProcessingOrder(QueueProcessingType.LIFO);
+        config.writeDebugLogs(); // Remove for release app
+
+        // Initialize ImageLoader with configuration.
+        ImageLoader.getInstance().init(config.build());
+
+    }
+}
--- app/src/main/java/edu/temple/androidcontext/MainActivity.java
+++ app/src/main/java/edu/temple/androidcontext/MainActivity.java
@@ -3,12 +3,17 @@ package edu.temple.androidcontext;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class MainActivity extends Activity {
 
     LinearLayout ll;
+
+    EditText imageURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +21,15 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         ll = (LinearLayout) findViewById(R.id.imageContainer);
+        imageURL = (EditText) findViewById(R.id.imageURL);
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ImageView image = new ImageView(MainActivity.this);
                 ll.addView(image);
+
+                ImageLoader.getInstance().displayImage(imageURL.getText().toString(), image);
             }
         });
 
