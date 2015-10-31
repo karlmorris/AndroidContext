d865cffe1d62aa10e79fde78a4b7b8defa2d67ff
Karl Morris <siliclone@gmail.com>
2015-10-31 11:41:26 -0400

    Using Picasso with local context for dynamic image loading.

--- app/src/main/java/edu/temple/androidcontext/MainActivity.java
+++ app/src/main/java/edu/temple/androidcontext/MainActivity.java
@@ -3,25 +3,52 @@ package edu.temple.androidcontext;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends Activity {
 
     LinearLayout ll;
+    EditText imageURL;
+    CheckBox cacheCheckBox;
+    boolean shouldUseCache = true;
+    int cacheCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         ll = (LinearLayout) findViewById(R.id.imageContainer);
+        imageURL = (EditText) findViewById(R.id.imageURL);
+        cacheCheckBox = (CheckBox) findViewById(R.id.checkBox);
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ImageView image = new ImageView(MainActivity.this);
                 ll.addView(image);
+
+                String url = imageURL.getText().toString();
+
+                if (!shouldUseCache)
+                    url = url.concat("?" + String.valueOf(cacheCounter++));
+
+                // Picasso singleton with activity (local) context
+                Picasso.with(MainActivity.this).load(url).into(image);
+            }
+        });
+
+        cacheCheckBox.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                shouldUseCache = isChecked;
             }
         });
 
